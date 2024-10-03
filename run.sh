886cb47ac541226565691e8986a71ceda0bc3fa7
juanjaramillo98 <juan.jaramillo62@udea.edu.co>
2024-10-02 21:36:08 -0500

    modificar run.sh para tenner encuenta la nueva carpeta

--- run.sh
+++ run.sh
@@ -4,13 +4,12 @@
 
 cd
 
-cd /home/spark/tensorflow_cluster
-
+cd /home/spark
 ls
 
 /opt/bitnami/spark/bin/spark-submit     \
   --class org.apache.spark.examples.SparkPi     \
-  --conf spark.kubernetes.container.image=bitnami/spark:3     \
-  --master spark://my-release-spark-master-svc.default.svc.cluster.local:7077    \
-  --conf spark.kubernetes.driverEnv.SPARK_MASTER_URL=spark://my-release-spark-master-svc.default.svc.cluster.local:7077       \
+  --conf spark.kubernetes.container.image=bitnami/spark:3 \
+  --master spark://my-release-spark-master-svc.default.svc.cluster.local:7077 \
+  --conf spark.kubernetes.driverEnv.SPARK_MASTER_URL=spark://my-release-spark-master-svc.default.svc.cluster.local:7077 \
   $1
